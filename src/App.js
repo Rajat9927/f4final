c6ed5fe216fe4f1bfcc798c0c63461c90bb016f3
Rajat9927 <Rajat9927kumar@gmail.com>
2023-05-05 23:50:12 +0530

    fullfinal

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Home from "./Components/Home";
 import Profile from "./Components/Profile";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    alert("My code is working perfectly on Local server, it is not working correctly after hosting on Github, Please Click HomePage Button to load data.After that it will work fine, I can show you on my local serverr, Please call or whatsapp 9927941725, That's why I have added this nav bar to make homepage accesbile, but in local server it it working fine.")
+})
   return (
     <div className="App">
       <BrowserRouter>
